ff571703171832575afa9a33e270c582d81637d7
mario-g-g <mario.gil@civica-soft.com>
2024-03-06 09:44:29 +0100

    Fin seccion 11

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+import { Menu, MenuItemContent } from 'primeng/menu';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    PrimeNgModule
+  ],
+  exports: [
+    MenuComponent,
+  ]
+})
+export class SharedModule { }
